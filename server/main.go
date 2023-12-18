e4b0cbdaaae96896e731c6afcd4879dbe1a67ff6
hclanka <152237163+chandudagoat@users.noreply.github.com>
2023-12-19 00:22:12 +0530

    added addition proto for the grpc api

---
+++ server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"calc/pb"
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) Add(
+	ctx context.Context, in *pb.CalcRequest,
+) (*pb.CalcResponse, error) {
+	return &pb.CalcResponse{
+		Result: in.A + in.B,
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("failed to create listener", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	pb.RegisterCalculatorServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatal("failed to serve", err)
+	}
+}
